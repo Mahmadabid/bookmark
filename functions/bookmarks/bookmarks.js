e8241832653090730f50b99cbf362b98b43197dc
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-05-13 02:18:05 -0700

    added

--- functions/bookmarks/bookmarks.js
+++ functions/bookmarks/bookmarks.js
@@ -11,40 +11,94 @@ type Query {
 }
 type Bookmark {
   id: ID!
-  desc: String!
+  name: String!
   url: String!
 }
 type Mutation{
-  addBookmark(desc: String!, url: String!): Bookmark
-  editBookmark(desc: String!, url: String!): Bookmark
+  addBookmark(name: String!, url: String!): Bookmark
+  editBookmark(name: String!, url: String!): Bookmark
+  delBookmark(id: ID!): Bookmark
 }
 `
 
-const bookMark = [
-  { id: 1, desc: 'Terry Pratchett', url="" },
-  { id: 2, desc: 'Stephen King', url="" },
-  { id: 3, desc: 'JK Rowling', url="" },
-]
-
 const resolvers = {
   Query: {
-    bookmarks: (root, args, context) => {
-      return bookMark
+    bookmarks: async (parent, args, { user }) => {
+      if (!user) {
+        return [];
+      } else {
+        const results = await client.query(
+          q.Paginate(q.Match(q.Index("user_bookmark"), user))
+        );
+        return results.data.map(([ref, name, url]) => {
+          return {
+            id: ref.id,
+            name,
+            url,
+          };
+        });
+      }
     },
   },
   Mutation: {
-    addBookmark: async(_, {desc, url}, {user}) => {
+    addBookmark: async (_, { name, url }, { user }) => {
       if (!user) {
         return "You are not authorized"
       }
+      const results = await client.query(
+        q.Create(q.Collection("bookmark"), {
+          data: {
+            name,
+            owner: user,
+            url,
+          },
+        })
+      );
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
+    },
 
-    }
+    delBookmark: async (_, { id }, { user }) => {
+      if (!user) {
+        throw new Error("Must be authenticated to delete bookmarks");
+      }
+      const results = await client.query(
+        q.Delete(q.Ref(q.Collection("bookmark"), id))
+      );
+    },
+
+    editBookmark: async (_, { id, url, name }, { user }) => {
+      if (!user) {
+        throw new Error("Must be authenticated to add bookmarks");
+      }
+      const results = await client.query(
+        q.Update(q.Ref(q.Collection("bookmark"), id), {
+          data: {
+            name,
+            url,
+          },
+        })
+      );
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
+    },
   }
 }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ context }) => {
+    if (context.clientContext.user) {
+      return { user: context.clientContext.user.sub };
+    } else {
+      return {};
+    }
+  },
 })
 
 const handler = server.createHandler()
--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,110 +1,158 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import { IconButton, ListItem, ListItemText } from '@material-ui/core';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Favorite from '@material-ui/icons/Favorite';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import EditIcon from '@material-ui/icons/Edit';
-import { GET_TODO } from './TaskBox';
+// import { GET_TODO } from './TaskBox';
+// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+// import { StringLiteralLike } from 'typescript';
+import LinkIcon from '@material-ui/icons/Link';
 
 interface BookmarkProps {
-    task: string
+    name: string
     id: string
-    status: boolean
-    date: string,
-    setCheckLoading: Dispatch<SetStateAction<boolean>>
-    setUpdateLoading: Dispatch<SetStateAction<boolean>>
+    url: String
+    setEditLoading: Dispatch<SetStateAction<boolean>>
     setDelLoading: Dispatch<SetStateAction<boolean>>
 }
 
-const DEL_TODO = gql`
-  mutation delTodo($id: ID!) {
-    delTodo(id: $id) {
+const DEL_BOOKMARK = gql`
+  mutation delBookmark($id: ID!) {
+    delBookmark(id: $id) {
       id
     }
   }
 `;
 
-const CHECK_TODO = gql`
-    mutation checkTodo($id: ID!, $status: Boolean!) {
-        checkTodo(id: $id, status: $status) {
+const EDIT_BOOKMARK = gql`
+    mutation editBookmark($id: ID!, $name: String!, $url: String!) {
+        editBookmark(id: $id, name: $name, url: $url) {
             id,
-            status
+            name,
+            url
         }
     }
 `;
 
-const UPDATE_TODO = gql`
-    mutation UpdateTodo($id: ID!, $task: String!) {
-        updateTodo(id: $id, task: $task) {
-            id,
-            task
-        }
-    }
-`;
+const Bookmark: React.FC<BookmarkProps> = ({ setDelLoading, setEditLoading, name, id, url }) => {
 
-const Bookmark: React.FC<BookmarkProps> = ({ setCheckLoading, setDelLoading, setUpdateLoading, task, id, status, date }) => {
+    const [open, setOpen] = useState(false);
+    // const [NameError, setNameError] = useState(false);
+    // const [UrlError, setUrlError] = useState(false);
 
-    const [delTodo, { loading: delloading }] = useMutation(DEL_TODO);
-    const [checkTodo, { loading: checkloading }] = useMutation(CHECK_TODO);
-    const [updateTodo, { loading: updateloading }] = useMutation(UPDATE_TODO);
+    // const handleClickOpen = () => {
+    //     setOpen(true);
+    // };
+
+    // const handleClose = () => {
+    //     setOpen(false);
+    // };
+
+    const [delBookmark, { loading: delloading }] = useMutation(DEL_BOOKMARK);
+    const [editBookmark, { loading: editloading }] = useMutation(EDIT_BOOKMARK);
 
     React.useEffect(() => {
-        setCheckLoading(checkloading);
         setDelLoading(delloading);
-        setUpdateLoading(updateloading);
-    }, [checkloading, delloading, updateloading])
+        setEditLoading(editloading);
+    }, [delloading, editloading])
 
-    const DelTask = () => {
-        delTodo({
+    const DelBookmark = () => {
+        delBookmark({
             variables: {
                 id,
             },
-            refetchQueries: [{ query: GET_TODO }],
+            // refetchQueries: [{ query: GET_TODO }],
         });
     }
 
-    const CheckTask = () => {
-        status = !status
-        checkTodo({
-            variables: {
-                id,
-                status
-            },
-            refetchQueries: [{ query: GET_TODO }],
-        });
-    }
-
-    const UpdateTask = () => {
-        const value = prompt("Enter new name");
-        if (value === "") {
-            alert("Enter a value");
+    const EditBookmark = () => {
+        const Name = prompt("Enter name");
+        if (Name === "") {
+            alert("Enter name");
+        }
+        const Url = prompt("Enter url");
+        if (Url === "") {
+            alert("Enter url");
         }
         else {
-            updateTodo({
+            editBookmark({
                 variables: {
                     id,
-                    task: value
+                    url: Url,
+                    name: Name
                 },
-                refetchQueries: [{ query: GET_TODO }],
+                // refetchQueries: [{ query: GET_TODO }],
             });
         }
     }
 
+    // const EditButton = () => {
+    //     return (
+    //         <div>
+    //             <EditIcon style={{ marginRight: '20px', cursor: 'pointer' }} onClick={handleClickOpen} />
+    //             <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+    //                 <DialogTitle id="form-dialog-title">Edit</DialogTitle>
+    //                 <DialogContent>
+    //                     <DialogContentText>
+    //                         To change the name and url enter the values and edit
+    //                 </DialogContentText>
+    //                     <TextField
+    //                         autoFocus
+    //                         margin="dense"
+    //                         id="name"
+    //                         label="name"
+    //                         type="text"
+    //                         fullWidth
+    //                         onChange={(e) => setName(e.target.value)}
+    //                         error={NameError} helperText={NameError ? 'Empty field!' : ' '}
+    //                     />
+    //                     <TextField
+    //                         autoFocus
+    //                         margin="dense"
+    //                         id="url"
+    //                         label="url"
+    //                         type="url"
+    //                         fullWidth
+    //                         onChange={(e) => setUrl(e.target.value)}
+    //                         error={UrlError} helperText={UrlError ? 'Empty field!' : ' '}
+    //                     />
+    //                 </DialogContent>
+    //                 <DialogActions>
+    //                     <Button onClick={handleClose} color="primary">
+    //                         Cancel
+    //                     </Button>
+    //                     <Button onClick={EditBookmark} color="primary">
+    //                         Edit
+    //                     </Button>
+    //                 </DialogActions>
+    //             </Dialog>
+    //         </div>
+    //     )
+    // }
+
     return (
         <ListItem>
-            <IconButton onClick={CheckTask} color="inherit">
-                {status ? <Favorite color="primary" /> : <FavoriteBorder />}
-            </IconButton>
-            <ListItemText primary={task} secondary={date} />
-            <EditIcon style={{ marginRight: '20px', cursor: 'pointer' }} onClick={UpdateTask} />
+            <a href={'string'} target="blank">
+                <IconButton color="inherit">
+                    <LinkIcon />
+                </IconButton>
+            </a>
+            <ListItemText primary={name} secondary={url} />
+            {/* <EditButton /> */}
+                <EditIcon style={{ marginRight: '20px', cursor: 'pointer' }} onClick={EditBookmark} />
             <FormControlLabel
                 control={
                     <Checkbox
                         checked={true}
-                        onChange={DelTask}
+                        onChange={DelBookmark}
                         name="checked"
                         indeterminate
                     />
